704fc13e0b249b14719aad2a316dbe109291c0ec
Jinkyu Han <hjk1019@gmail.com>
2021-07-15 21:50:09 +0900

    '템플릿 엔진 사용하기'에 대한 내용 추가

--- docs/06/learn-express/app.js
+++ docs/06/learn-express/app.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const dotenv = require('dotenv');
 const path = require('path');
+const nunjucks = require('nunjucks')
 
 dotenv.config();
 const indexRouter = require('./routes')
@@ -11,7 +12,12 @@ const userRouter = require('./routes/user')
 const app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'pug');
+app.set('view engine', 'html');
+
+nunjucks.configure('views', {
+    express: app,
+    watch: true
+});
 
 app.use(morgan('dev'));
 app.use('/', express.static(path.join(__dirname, 'public')));
@@ -33,12 +39,16 @@ app.use('/', indexRouter);
 app.use('/user', userRouter);
 
 app.use((req, res, next) => {
-    res.status(404).send('Not Found');
+    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다`);
+    error.status = 404;
+    next(error);
 });
 
 app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).send(err.message);
+    res.locals.message = err.message;
+    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 const multer = require('multer');
