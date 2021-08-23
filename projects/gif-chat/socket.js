234136874f646d82d3b10fd1f9d9b83c85327cd6
Jinkyu Han <hjk1019@gmail.com>
2021-08-23 21:55:32 +0900

    '실시간 GIF 채팅방 만들기'에 대한 내용 추가

--- projects/gif-chat/socket.js
+++ projects/gif-chat/socket.js
@@ -1,26 +1,32 @@
 const SocketIO = require('socket.io');
 
-module.exports = (server) => {
+module.exports = (server, app) => {
     const io = SocketIO(server, { path: '/socket.io' });
 
-    io.on('connection', (socket) => {
+    app.set('io', io);
+
+    const room = io.of('/room');
+    const chat = io.of('/chat');
+
+    room.on('connection', (socket) => {
+        console.log('room 네임스페이스에 접속');
+        socket.on('disconnect', () => {
+            console.log('room 네임스페이스 접속 해제');
+        });
+    });
+
+    chat.on('connection', (socket) => {
+        console.log('chat 네임스페이스에 접속');
         const req = socket.request;
-        const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-        console.log('새로운 클라이언트 접속', ip, socket.id, req.ip);
+        const { headers: { referer } } = req;
+        const roomId = referer
+            .split('/')[referer.split('/').length - 1]
+            .replace(/\?.+/, '')
+        socket.join(roomId);
 
         socket.on('disconnect', () => {
-            console.log('클라이언트 접속 해제', ip, socket.id);
-            clearInterval(socket.interval);
+            console.log('chat 네임스페이스 접속 해제');
+            socket.leave(roomId);
         });
-        socket.on('error', (error) => {
-            console.error(error);
-        });
-        socket.on('reply', (data) => {
-            console.log(data.toString());
-        });
-
-        socket.interval = setInterval(() => {
-            socket.emit('news', 'Hello Socket.IO');
-        }, 3 * 1000);
     });
 };
