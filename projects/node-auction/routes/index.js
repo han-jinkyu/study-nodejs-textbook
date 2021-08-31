d3a59a4aa5909f87029568a93c5e48bc4bed7102
Jinkyu Han <hjk1019@gmail.com>
2021-08-31 22:16:29 +0900

    '프로젝트 마무리하기'에 대한 내용 추가

--- projects/node-auction/routes/index.js
+++ projects/node-auction/routes/index.js
@@ -157,4 +157,23 @@ router.post('/good/:id/bid', isLoggedIn, async (req, res, next) => {
     }
 });
 
+router.get('/list', isLoggedIn, async (req, res, next) => {
+    try {
+        const goods = await Good.findAll({
+            where: { SoldId: req.user.id },
+            include: { model: Auction },
+            order: [
+                [ { model: Auction }, 'bid', 'DESC' ],
+            ],
+        });
+        res.render('list', { 
+            title: '낙찰 목록 - NodeAuction', 
+            goods 
+        });
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
+});
+
 module.exports = router;
